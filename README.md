401276941103f891faa6924d95b82ac148a38965
MaxVidom <MaxVidomenko@gmail.com>
2022-09-17 22:22:45 +0300

    Complete all tasks

---
+++ README.md
@@ -0,0 +1,7 @@
+1. https://ibb.co/fXyzNLV
+
+2) https://ibb.co/ZYkhj6Z
+
+3) https://ibb.co/HnhCjSc
+
+4) https://ibb.co/Gs4Ys2h
---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const updateContactsData = async (contacts) => await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+async function listContacts() {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const contact = contacts.find(({id}) => id === contactId);
+    return contact || null;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+    if (contactIndex === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(contactIndex, 1);
+    await updateContactsData(contacts);
+    return result;
+
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
+    contacts.push(newContact);
+    await updateContactsData(contacts);
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
